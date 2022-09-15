e8e860ad84029d77d8028e33011c9594734ec864
ArdenHide <ardenhide@gmail.com>
2022-09-15 22:18:04 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RQTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> styles = new List<string> { "Orange", "Blue" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "Blue";
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+
+            ChangeThemeFile("Brushes");
+            ChangeThemeFile("Core");
+            ChangeThemeFile("Icons");
+        }
+
+        private void ChangeThemeFile(string fileName)
+        {
+            string style = styleBox.SelectedItem as string;
+            // определяем путь к файлу ресурсов
+            var uri = new Uri("/Themes/" + style + "/" + fileName + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+    }
+}
