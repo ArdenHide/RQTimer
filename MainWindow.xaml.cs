88dd0f40ba43c9719fdf679d261ac7ee06cf15e1
ArdenHide <ardenhide@gmail.com>
2022-10-02 12:55:21 +0300

    remove Themes, add MD

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,32 +23,6 @@ namespace RQTimer
         public MainWindow()
         {
             InitializeComponent();
-
-            List<string> styles = new List<string> { "Orange", "Blue" };
-            styleBox.SelectionChanged += ThemeChange;
-            styleBox.ItemsSource = styles;
-            styleBox.SelectedItem = "Blue";
-        }
-
-        private void ThemeChange(object sender, SelectionChangedEventArgs e)
-        {
-            // очищаем коллекцию ресурсов приложения
-            Application.Current.Resources.Clear();
-
-            ChangeThemeFile("Brushes");
-            ChangeThemeFile("Core");
-            ChangeThemeFile("Icons");
-        }
-
-        private void ChangeThemeFile(string fileName)
-        {
-            string style = styleBox.SelectedItem as string;
-            // определяем путь к файлу ресурсов
-            var uri = new Uri("/Themes/" + style + "/" + fileName + ".xaml", UriKind.Relative);
-            // загружаем словарь ресурсов
-            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
-            // добавляем загруженный словарь ресурсов
-            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
         }
     }
 }
